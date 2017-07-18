37ea3357208760f3a74c78a8df2fab70782dccac
荒井俊洋 <araitoshihiro@TA-MacBook-Air.local>
2017-07-19 02:00:29 +0900

    Sec 6 60 done

---
+++ DoIt/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  DoIt
+//
+//  Created by 荒井俊洋 on 2017/07/19.
+//  Copyright © 2017年 荒井俊洋. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name = ""
+    var important = false
+}
